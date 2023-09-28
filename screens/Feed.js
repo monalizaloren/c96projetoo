f57f72945c985745a2b82c278eb2cb968f642047
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-09-28 10:49:23 -0300

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -9,35 +9,7 @@ import {
     Image
 } from "react-native";
 
-import PostCard from "./PostCard";
-import { FlatList } from "react-native-gesture-handler";
-
-let posts = require("./temp_posts.json");
-
-export default class Feed extends Component {
-
-    renderItem = ({ item: post }) => {
-        return <PostCard post={post} />;
-    };
-
-    keyExtractor = (item, index) => index.toString();
-
-    render() {
-      return (
-        <View style={styles.container}>
-          <SafeAreaView style={styles.droidSafeArea} />
-          <View style={styles.appTitle}>
-            <Text style={styles.appTitleText}>Espectagrama</Text>
-          </View>
-          <View style={styles.cardContainer}>
-            <FlatList
-              keyExtractor={this.keyExtractor}
-              data={posts}
-              renderItem={this.renderItem}
-            />
-          </View>
-        </View>
-      );
+//PASSO 1
     }
 }
 
