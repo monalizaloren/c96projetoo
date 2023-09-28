808954091eb9b5f72ae7ffc1451d05e897358c12
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-09-28 10:50:40 -0300

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -12,26 +12,4 @@ import {
 //PASSO 1
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "darkblue"
-    },
-    droidSafeArea: {
-        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 35
-    },
-    appTitle: {
-        flex: 0.07,
-        padding: 5,
-        flexDirection: "row",
-        justifyContent: "center"
-    },
-    appTitleText: {
-        color: "white",
-        fontSize: 28,
-    },
-    cardContainer: {
-        flex: 0.93
-    }
-});
+//PASSO 2
